915616edd196409cc55c95890b03de8163739b08
julioconceicao <julioconceicao.gtr@gmail.com>
2024-04-22 00:06:17 -0300

    feat: modify app files and folders

---
+++ my-app/src/app/home/index.jsx
@@ -0,0 +1,12 @@
+import {View, Text} from "react-native"
+import {styles} from "./index-style"
+
+export function Home(){
+    return(
+        <View>
+            <Text>
+                Texto 1
+            </Text>
+        </View>
+    )
+}
